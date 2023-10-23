5c2036947c7caccba9f35f7ed9762125af841b90
student <chrisscotthw@gmail.com>
2023-10-23 16:24:21 -0400

    10/23 update

--- src/scripts/productController.js
+++ src/scripts/productController.js
@@ -1,9 +1,17 @@
 import mongoose from 'mongoose';
-const itemSchema = require('')
+const itemSchema = require('..models/items.model.js');
 
 const mongo = require('mongoose');
 
 const fillProductPage = async (req, res) => {
-    //const item = mongoose.model
+    const Item = mongoose.model('Product', itemSchema);
+    const {_id} = req.body;
+    const item = await JSON.parse(Item.findOneById({_id}));
+
+    console.log(item);
+
+    return res.item
 }
+
+export default fillProductPage;
 
